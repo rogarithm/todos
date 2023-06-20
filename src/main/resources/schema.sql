64e21878e1ba9e5a9154e43ec237b34d78eb7920
sehun-kim <rogarithm@gmail.com>
2023-06-20 16:35:22 +0900

    프로젝트 초기 설정

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `user` (
+  `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `account` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `crn` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE `todo` (
+  `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now())
+);

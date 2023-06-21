9ba58b6e065e05abac485ad6d9992ff909629d71
sehun-kim <rogarithm@gmail.com>
2023-06-21 21:31:53 +0900

    불필요한 주석을 삭제한다

--- src/main/java/kr/rogarithm/todos/domain/todo/dto/TodoResponse.java
+++ src/main/java/kr/rogarithm/todos/domain/todo/dto/TodoResponse.java
@@ -8,12 +8,6 @@ import lombok.Getter;
 @Builder
 public class TodoResponse {
 
-    /**
-     * state
-     * ALL : 완료된 항목과 미완료된 항목 모두
-     * COMPLETE : 완료된 항목만
-     * INCOMPLETE : 미완료된 항목만
-     */
     private Long id;
     private String name;
     private String description;

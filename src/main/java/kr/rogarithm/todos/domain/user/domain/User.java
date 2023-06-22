130c5bf9144071a90673b250ab6edacc9b839dd4
sehun-kim <rogarithm@gmail.com>
2023-06-23 00:54:39 +0900

    테스트 오류를 고친다
     - 구현한 테스트 중 findUserByAccountSuccess() 실행 시
       DataIntegrityViolationException이 발생했다.
     - 테스트에서 쓰는 User 엔티티에 @Builder를 선언했는데, 그러면
       매개변수가 없는 생성자가 생기지 않는 것이 문제였다.
     - @NoArgsConstructor를 추가하면 다시 오류가 발생하는데, @Builder를
       클래스 레벨에 선언하면 모든 필드를 매개변수로 갖는 생성자가 필요하기
       때문이다. (lombok 문서의 @Builder 참조)
     - 결론적으로 @NoArgsConstructor와 @AllArgsConstructor를 추가해 오류를
       고쳤다.

--- src/main/java/kr/rogarithm/todos/domain/user/domain/User.java
+++ src/main/java/kr/rogarithm/todos/domain/user/domain/User.java
@@ -1,11 +1,15 @@
 package kr.rogarithm.todos.domain.user.domain;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     private Long id;

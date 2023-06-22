9a6065648ec480a92cc02540a4195aa994d1b7ed
sehun-kim <rogarithm@gmail.com>
2023-06-22 23:29:55 +0900

    테스트 코드를 정리한다

--- src/main/java/kr/rogarithm/todos/domain/user/dto/JoinUserRequest.java
+++ src/main/java/kr/rogarithm/todos/domain/user/dto/JoinUserRequest.java
@@ -8,21 +8,21 @@ import lombok.Getter;
 @Builder
 public class JoinUserRequest {
 
-  private String account;
-  private String password;
-  private String nickname;
-  private String phone;
-  private String crn;
+    private String account;
+    private String password;
+    private String nickname;
+    private String phone;
+    private String crn;
 
-  public static JoinUserRequest of(User user) {
-      return JoinUserRequest.builder()
-                            .account(user.getAccount())
-                            .password(user.getPassword())
-                            .nickname(user.getNickname())
-                            .phone(user.getPhone())
-                            .crn(user.getCrn())
-                            .build();
-  }
+    public static JoinUserRequest of(User user) {
+        return JoinUserRequest.builder()
+                              .account(user.getAccount())
+                              .password(user.getPassword())
+                              .nickname(user.getNickname())
+                              .phone(user.getPhone())
+                              .crn(user.getCrn())
+                              .build();
+    }
 
     public User toUser() {
         return User.builder()

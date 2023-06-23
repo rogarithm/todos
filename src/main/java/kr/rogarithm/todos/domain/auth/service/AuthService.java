75759ea41fb5310cd02d4bcae97e216f862a1697
sehun-kim <rogarithm@gmail.com>
2023-06-23 17:46:02 +0900

    AuthService는 비밀번호가 일치하지 않는 로그인 요청을 처리할 수 있다

--- src/main/java/kr/rogarithm/todos/domain/auth/service/AuthService.java
+++ src/main/java/kr/rogarithm/todos/domain/auth/service/AuthService.java
@@ -4,6 +4,7 @@ import kr.rogarithm.todos.domain.auth.dto.LoginRequest;
 import kr.rogarithm.todos.domain.auth.dto.LoginResponse;
 import kr.rogarithm.todos.domain.auth.exception.AuthenticationFailedException;
 import kr.rogarithm.todos.domain.user.dao.UserMapper;
+import kr.rogarithm.todos.domain.user.domain.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,9 +19,15 @@ public class AuthService {
     public LoginResponse loginUser(LoginRequest request) {
 
         String account = request.getAccount();
+        String password = request.getPassword();
+        User user = userMapper.selectUserByAccount(account);
 
-        if (userMapper.selectUserByAccount(account) == null) {
+        if (user == null) {
             throw new AuthenticationFailedException("입력한 계정(" + account + ")으로 가입된 회원 정보가 없습니다");
+        }
+
+        if (!user.getPassword().equals(password)) {
+            throw new AuthenticationFailedException("잘못된 비밀번호입니다");
         }
 
         return null;
--- src/test/java/kr/rogarithm/todos/domain/auth/service/AuthServiceTest.java
+++ src/test/java/kr/rogarithm/todos/domain/auth/service/AuthServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import kr.rogarithm.todos.domain.auth.dto.LoginRequest;
 import kr.rogarithm.todos.domain.auth.exception.AuthenticationFailedException;
 import kr.rogarithm.todos.domain.user.dao.UserMapper;
+import kr.rogarithm.todos.domain.user.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -37,4 +38,26 @@ class AuthServiceTest {
         verify(userMapper).selectUserByAccount(request.getAccount());
     }
 
+    @Test
+    public void failLoginWhenPasswordIsNotMatched() {
+
+        LoginRequest request = LoginRequest.builder()
+                                           .account("sehoongim")
+                                           .password("not-matched")
+                                           .build();
+
+        User user = User.builder()
+                        .account("sehoongim")
+                        .password("q1w2e3!")
+                        .nickname("shrimp-cracker")
+                        .phone("010-1010-1010")
+                        .crn("123-45-67890")
+                        .build();
+
+        when(userMapper.selectUserByAccount(request.getAccount())).thenReturn(user);
+
+        Assertions.assertThrows(AuthenticationFailedException.class, () -> authService.loginUser(request));
+
+        verify(userMapper).selectUserByAccount(request.getAccount());
+    }
 }

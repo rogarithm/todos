da30c95254cf8f959ceccb3dcaf7ca57aa5bf505
sehun-kim <rogarithm@gmail.com>
2023-06-25 07:09:43 +0900

    Authorization 처리를 해줄 필터를 구현한다

---
+++ src/main/java/kr/rogarithm/todos/global/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,87 @@
+package kr.rogarithm.todos.global.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtAuthenticationManager jwtAuthenticationManager;
+
+    private final List<String> excludedPaths = List.of(
+            "/auth/login", "/verify/account", "/verify/crn", "/verify/nickname", "/user"
+    );
+
+    public JwtAuthenticationFilter(JwtAuthenticationManager jwtAuthenticationManager) {
+        this.jwtAuthenticationManager = jwtAuthenticationManager;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if (excludedPaths.contains(request.getServletPath())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+
+            response.getWriter().println("{ \"message\": \"" + "인증을 위해 JWT 토큰이 필요합니다." + "\" }");
+
+            return;
+        }
+
+        String token = authHeader.substring("Bearer ".length());
+
+        try {
+            jwtAuthenticationManager.verifyToken(token);
+        } catch (ExpiredJwtException e) {
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+
+            ResponseStatusException responseStatusException = new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다.");
+
+            mapper.writeValue(response.getWriter(), responseStatusException);
+
+        } catch (JwtException e) {
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+
+            ResponseStatusException responseStatusException = new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED, "잘못된 토큰 형식입니다.");
+
+            mapper.writeValue(response.getWriter(), responseStatusException);
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/kr/rogarithm/todos/global/auth/JwtAuthenticationManager.java
@@ -0,0 +1,52 @@
+package kr.rogarithm.todos.global.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationManager {
+
+    private static final String secretKey = "TodosAppSecretLengthIsMoreThan256";
+
+    public Claims verifyToken(String token) {
+        return Jwts.parserBuilder()
+                   .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                   .build()
+                   .parseClaimsJws(token)
+                   .getBody();
+    }
+
+    public String getUserIdByToken(String token) {
+        return getBodyByToken(token).getSubject();
+    }
+
+    public long getExpirationDayByToken(String token) {
+        long now = new Date(System.currentTimeMillis()).getTime();
+        long expiration = getExpirationByToken(token).getTime();
+
+        return TimeUnit.DAYS.convert((expiration - now), TimeUnit.MILLISECONDS);
+    }
+
+    public Date getExpirationByToken(String token) {
+        return getBodyByToken(token).getExpiration();
+    }
+
+    private Key getSecretKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    private Claims getBodyByToken(String token) throws JwtException {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
